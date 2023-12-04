1c27a6113727d52a099b12be3b966f010bfb653d
bhupesh99000 <bhupi.99000@gmail.com>
2023-12-05 00:28:40 +0530

    footer complete
    Modifications for footer complete

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,4 +1,6 @@
 import { readBlockConfig, decorateIcons } from '../../scripts/aem.js';
+import utils from '../../scripts/utils.js';
+import socialIconBlock from '../social-icons/social-icons.js';
 
 /**
  * loads and decorates the footer
@@ -18,14 +20,40 @@ export default async function decorate(block) {
     // decorate footer DOM
     const footer = document.createElement('div');
     footer.innerHTML = html;
+    let isFound = false;
+    const footerElms = [...footer.children[0].children].reduce(((elements, curEl) => {
+      const currentContent = curEl.textContent.trim() || curEl.innerHTML;
+      if (currentContent === 'Ⓒ 2019, WKND Site.') {
+        isFound = true;
+      }
 
-    // [...footer.children[0].children].forEach((element, index) => {
-    //   console.log(element);
-    // });
-    // [...footer.children[0].children].reduce(((prevItem, curItem, index, array) => {
-    //   // console.log(prevItem, curItem, index, array);
+      if (currentContent && !isFound) {
+        elements.push(curEl);
+      }
+      return elements;
+    }), []);
 
-    // }), []);
+    const footerElmsParent = document.createElement('div');
+    footerElmsParent.classList.add('footer-links');
+    utils.wrapAll(footerElms, footerElmsParent);
+
+    const footerLinkEls = [...footerElmsParent.children];
+    const halfLength = Math.ceil(footerLinkEls.length / 2);
+
+    const firstElSet = footerLinkEls.splice(0, halfLength);
+    const secondElSet = footerLinkEls;
+
+    const footerLinksLeft = document.createElement('div');
+    footerLinksLeft.classList.add('footer-links-left');
+    const footerLinksRight = document.createElement('div');
+    footerLinksRight.classList.add('footer-links-right');
+
+    utils.wrapAll(firstElSet, footerLinksLeft);
+    utils.wrapAll(secondElSet, footerLinksRight);
+
+    const socialIconsWrapper = footerLinksRight.children[1];
+    socialIconsWrapper.classList.add('social-icons-parent');
+    socialIconBlock(socialIconsWrapper, 'footer');
 
     decorateIcons(footer);
     block.append(footer);
